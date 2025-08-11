c72c296a89566eb776642373429ed4df4a4b9913
Vanessa Targino <nessaoli1996@gmail.com>
2025-08-10 22:37:57 -0300

    Configurações iniciais tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,38 +3,26 @@ import logo from '../../assets/logo.svg'
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center items-center" >
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <h1>Bem-vindo ao FarmaTech</h1>
-                        <h2>Inovação que cuida</h2>
-                    </div>
+                    <div>
+                        <div className="max-w-7xl flex flex-col items-center justify-center text-center" >
+                        
+                            <h1>Bem-vindo ao FarmaTech</h1>
+                            <h2>Inovação que cuida</h2>
+                        </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center",
-                         marginTop: "20px"
-                    }}>
-                        <img 
-                            src= {logo} 
-                            alt="Logo do projeto"
-                            style={{
-                                width: "100%",
-                                maxWidth: "300px",
-                                height: "auto"
-                            }} 
-                        />
+                        <div className= "max-w-7xl flex flex-col items-center justify-center text-center mt-4" >
+                            <img 
+                                src= {logo} 
+                                alt="Logo do projeto"
+                                style={{
+                                    width: "100%",
+                                    maxWidth: "300px",
+                                    height: "auto"
+                                }} 
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
